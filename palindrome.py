b01a59078ac9ab6ad5b3a21b929fded17358aeba
Shrigouri-h <shrigouri0707@gmail.com>
2026-01-18 18:43:20 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -4,9 +4,9 @@ def pal(text):
     return text == text[::-1]
 if __name__ == "__main__":
     if len(sys.argv)==2:
-        script_name = sys.argv[0]
+       
         text = sys.argv[1]
     else:
-        script_name = sys.argv[0]
+       
         text = "madam"
         print(pal(text))
